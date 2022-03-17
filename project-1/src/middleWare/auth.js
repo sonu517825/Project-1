1c24418938c3a41c62c9f0521e5754957ef2c23d
SONU VERMA <99230810+sonu517825@users.noreply.github.com>
2022-03-17 05:39:11 +0530

    Update auth.js

--- project-1/src/middleWare/auth.js
+++ project-1/src/middleWare/auth.js
@@ -2,41 +2,36 @@ const jwt = require('jsonwebtoken')
 const BlogsModel = require('../models/blogsModel')
 
 const authentication = async function (req, res, next) {
-
     try {
         let token = req.headers["x-api-key"]
 
-        if (!token) return res.status(400).send({ status: false, msg: "please provide token" })
+        if (!token) return res.status(400).send({ status: false, msg: "please provide token in request hadder" })
 
         let validateToken = jwt.verify(token, "projectgroup3")
 
         //! below condition execution chances are very low as catch block will handle the invalid token error
-        
+
         if (!validateToken) return res.status(401).send({ status: false, msg: "authentication failed" })
 
-        req['authenticateToken'] = token
-
+        res.setHeader('x-api-key', token)
         next()
-
     } catch (error) {
-        res.status(500).send({ error: error.message })
+        res.status(500).send({ status: false, error: error.message })
     }
 }
 
 
 const authorization = async function (req, res, next) {
 
-    let id = req.params.blogId
-    let jwtToken = req['authenticateToken']
     try {
+        let id = req.query.blogId
+        if (!id) return res.status(400).send({ status: false, msg: "please provide blog ID in query params" })
+
+        let token = req.headers["x-api-key"]
+        let verifiedToken = jwt.verify(token, "projectgroup3")
 
         let blogs = await BlogsModel.findById(id)
-
-        if (!blogs) return res.status(404).send({ status: false, msg: "please provide valid blog ID" })
-
-        if (blogs.isDeleted == true) return res.status(404).send({ status: false, msg: "no such blog found" })
-
-        let verifiedToken = jwt.verify(jwtToken, "projectgroup3")
+        if (!blogs) return res.status(404).send({ status: false, msg: "blog ID not exist" })
 
         if (verifiedToken.authorId != blogs.authorId) return res.status(403).send({ status: false, msg: "unauthorize access " })
 
