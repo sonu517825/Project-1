e86e38e01c412e82f7341fb909db9d0c8f0cbae4
SONU VERMA <99230810+sonu517825@users.noreply.github.com>
2022-03-17 05:35:06 +0530

    Update blogsModel.js

--- project-1/src/models/blogsModel.js
+++ project-1/src/models/blogsModel.js
@@ -5,7 +5,7 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 const blogSchema = new mongoose.Schema({
     title: { type: String, required: true },
     body: { type: String, required: true },
-    authorId: { type: ObjectId, ref: "Author" },
+    authorId: { type: ObjectId, ref: "Author", required: true},
     tags: [String],
     category: { type: String, required: true },
     subcategory: [{type : String}],
